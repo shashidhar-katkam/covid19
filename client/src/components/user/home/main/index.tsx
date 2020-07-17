e54217cfe80032910b87c62a7f1e74f1fd2f817c
Bimalendu Sarkar <bimalendu04@gmail.com>
2020-07-18 05:05:54 +0530

    Home page basic structure done.

--- client/src/components/user/home/main/index.tsx
+++ client/src/components/user/home/main/index.tsx
@@ -3,6 +3,7 @@ import {
     RouteComponentProps, withRouter
 } from 'react-router-dom';
 import './styles.scss';
+import styles from './styles.module.scss';
 import NewsService from '../../Service';
 import { INewsInfoC, MainNewsModel } from '../../../../models/models';
 import Actions from "../compose";
@@ -15,6 +16,15 @@ import { Shimmer3 } from "../../../common/Loading/Shimmers";
 import Analysis from "../templates/Analysis";
 import { debug } from "console";
 import Donations from "../Donations";
+import Highcharts from "highcharts";
+import HighchartsReact from "highcharts-react-official";
+import highchartsMap from "highcharts/modules/map";
+import mapDataIN from "@highcharts/map-collection/countries/in/custom/in-all-disputed.geo.json";
+import mapDataWorld from "@highcharts/map-collection/custom/world-eckert3-highres.geo.json";
+highchartsMap(Highcharts);
+
+const dataIndia = require('./dataIndia');
+const dataWorld = require('./dataWorld');
 
 interface IState {
 
@@ -37,12 +47,101 @@ class Main extends React.Component<IProps, IState> {
 
 
     render(): JSX.Element {
-        return (
-            <>
-                <div className="main-container-wrapper">
 
+        const mapOptionsIndia = {
+            chart: {
+                map: 'countries/in/custom/in-all-disputed',
+                backgroundColor: 'transparent'
+            },
+            title: {
+                text: 'Map Demo'
+            },
+            credits: {
+                enabled: false
+            },
+            mapNavigation: {
+                enabled: false
+            },
+            tooltip: {
+                headerFormat: '',
+                pointFormat: '<b>{point.freq}</b><br><b>{point.keyword}</b>                      <br>lat: {point.lat}, lon: {point.lon}'
+            },
+            series: [{
+                data: dataIndia,
+                name: 'Random data',
+                states: {
+                    hover: {
+                        color: '#BADA55'
+                    }
+                },
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                },
+
+                mapData: mapDataIN,
+            }]
+        }
+
+        const mapOptionsWorld = {
+            chart: {
+                map: 'countries/in/custom/in-all-disputed',
+                backgroundColor: 'transparent'
+            },
+            title: {
+                text: 'Map Demo'
+            },
+            credits: {
+                enabled: false
+            },
+
+            mapNavigation: {
+                enabled: false
+            },
+            tooltip: {
+                headerFormat: '',
+                pointFormat: '<b>{point.freq}</b><br><b>{point.keyword}</b>                      <br>lat: {point.lat}, lon: {point.lon}'
+            },
+            series: [{
+                data: dataWorld,
+                name: 'Random data',
+                states: {
+                    hover: {
+                        color: '#BADA55'
+                    }
+                },
+                dataLabels: {
+                    enabled: true,
+                    format: '{point.name}'
+                },
+
+                mapData: mapDataWorld,
+            }]
+        }
+
+        return (
+            <div className={styles.homePageContainer}>
+                    <div className={styles.parallax}></div>
+                    <div className={styles.wrapper}>
+                        <div className={styles.mapContainer}>
+                            <div className={styles.worldMap}>
+                                <HighchartsReact
+                                    constructorType={'mapChart'}
+                                    highcharts={Highcharts}
+                                    options={mapOptionsWorld}
+                                />
+                            </div>
+                            <div className={styles.indiaMap}>
+                                <HighchartsReact
+                                    constructorType={'mapChart'}
+                                    highcharts={Highcharts}
+                                    options={mapOptionsIndia}
+                                />
+                            </div>
+                        </div>
+                        <div></div>
+                    </div>
                 </div>
-            </>
         );
     }
 }
