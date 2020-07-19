9d73de4161177562e56472aa1002c1ee069897ec
shashidhar <shashi2puppy@gmail.com>
2020-07-19 15:28:56 +0530

    fd

--- client/src/components/user/home/Facts/factsData.ts
+++ client/src/components/user/home/Facts/factsData.ts
@@ -6,12 +6,14 @@ const factsData: any[] = [
         type: 'youtube'
     },{
         title: `What are the symptoms of COVID-19?`,
-        description: `The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don’t develop any symptoms and don't feel unwell. Most people (about 80%) recover from the disease without needing special treatment. Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. Older people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. People with fever, cough and difficulty breathing should seek medical attention. `,
+        description: `The most common symptoms of COVID-19 are fever, tiredness, and dry cough. Some patients may have aches and pains, nasal congestion, runny nose, sore throat or diarrhea. These symptoms are usually mild and begin gradually. Some people become infected but don’t develop any symptoms and don't feel unwell. Most people (about 80%) recover from the disease without needing special treatment. Around 1 out of every 6 people who gets COVID-19 becomes seriously ill and develops difficulty breathing. Older people, and those with underlying medical problems like high blood pressure, heart problems or diabetes, are more likely to develop serious illness. People with fever, cough and difficulty breathing should seek medical attention. Recently after WHO claimed new symptoms related to Covid-19 ie.Loss of smell (Ansomia) and Loss of taste.`,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/U8r3oTVMtQ0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `What should I do if I have COVID-19 symptoms and when should I seek medical care? `,
-        description: ``,
+        description: `If you have fever, cough or other above mentioned symptoms then there is a maximum probability that you have been infected by the virus. 
+
+        You should immediately contact Covid-19 helpline number.Even though your immune system can respond in stringent manner but you can be a carrier and spread it in large aspect. So you should Stay home, Take care of yourself.Get rest and stay hydrated. Stay in touch with your doctor. You should Avoid public transportation.As much as possible, stay in a specific room. Isolate yourself from other people and pets in your home. Wear mask (N95) or a cloth face covering, over your nose and mouth.  `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/t1z2XOvMDG0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
@@ -21,42 +23,56 @@ const factsData: any[] = [
         type: 'youtube'
     },{
         title: `Can COVID-19 be caught from a person who has no symptoms? `,
-        description: ``,
+        description: `Researchers suspect that there are undetected number of individuals who have mild or no symptoms, as per an article in Nature. That is on the grounds     that an expanding number of contaminated individuals have not ventured out to the hotspots, or come into contact with individuals       confirmed of COVID-  19. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/AOOtZLtZ9uo" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `How can we protect others and ourselves if we don't know who is infected? `,
-        description: ``,
+        description: `The virus is thought to spread mainly from person-to-person.Between people who are in close contact with one another (within about 6 feet).Everyone Should Wash your hands often with soap and water for at least 20 seconds especially after you have been in a public place, or after blowing your nose, coughing or sneezing. If soap and water are not readily available, use a hand sanitizer that contains at least 60% alcohol. Cover all surfaces of your hands and rub them together until they feel dry. Avoid touching your eyes, nose, and mouth with unwashed hands.Avoid close contact. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/f7J_Ca-N2IY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `What should I do if I have come in close contact with someone who has COVID-19? `,
-        description: ``,
+        description: `If you were just grocery shopping in the same store as someone who might have COVID-19, that would not be considered      a high risk — unless that person coughs or sneezes directly on you, which is a clear exposure. 
+
+        Quarantine Yourself  
+  
+        People who do come in close, significant contact with someone who have COVID-19 should self-quarantine for 14 days.    This involves   staying home and limiting your interactions with others — even if you don’t get sick. We now understand that people can have the virus and pass it on to others even if they aren’t showing any symptoms, so self-quarantining    is critical to helping prevent the spread. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/KZxffTan7bY" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `What does it mean to self-isolate? `,
-        description: ``,
+        description: `Self-isolation is about protecting others and slowing down the spread of COVID-19. It is very important that anyone who has the virus, or might have been exposed to it, limits the number of people they come into contact with for 14 days. This is the most effective way of preventing the coronavirus from spreading. 
+
+        If you are asked to self-isolate, it is important that you follow the advice which is there to help keep you, your loved ones,and your community safe. 
+  
+        Self-isolation may seem tricky at first, but across the country, hundreds of people have already successfully done it. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/zMpHqEb4v7c" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `What can I do to protect myself and prevent the spread of disease? `,
-        description: ``,
+        description: `Avoid close contact with people who are sick. Avoid touching your eyes, nose, and mouth. Stay home when you are sick. Cover your cough or sneeze with a tissue, then throw the tissue in the trash. Clean and disinfect frequently touched objects and surfaces every day. High touch surfaces include counters, tabletops, doorknobs, bathroom fixtures, toilets, phones, keyboards, tablets, and bedside tables. A list of products suitable for use against COVID-19 is available here. This list has been pre-approved by the US Environmental Protection Agency (EPA) for use during the COVID-19 outbreak. Wash your hands often with soap and water. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/lCABKB11vi4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `How to properly wear a medical mask? `,
-        description: ``,
+        description: `According to the World Health Organization, wearing a mask alone may not protect you against contracting the coronavirus. Still, when combined with proper hand cleaning and good hygiene, it can be effective. 
+
+        When Should You Wear a Medical Mask? 
+  
+        If you are healthy, you only need to wear a medical mask if you are taking care of someone who may be showing symptoms of the coronavirus. If you are coughing or sneezing, wearing a medical mask can reduce the spread of germs. A medical mask is only effective if you wash your hands frequently and properly with soap and water or hand sanitizer containing alcohol. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/adB8RW4I3o4" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `How to grocery shop safely? `,
-        description: ``,
+        description: `There is no current evidence that the COVID-19 virus is transmitted through food. 
+
+        Safety precautions help you avoid breathing in coronavirus or touching a contaminated surface and touching your face. In the grocery store, maintain at least six feet of distance between yourself and other shoppers. Wipe frequently touched remind you not to touch your face and can further help reduce spread of the virus. Use hand sanitizer before leaving the shop.If you are older than 65 or at increased risk for any reason, limit trips to the grocery store. Ask a neighbor or friend to pick up groceries and leave them outside your house. See if your grocery store offers special hours for older adults or those with underlying conditions. Or have groceries delivered to your home. `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/taOUVUp1Pqw" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },{
         title: `How to wash fruits and vegetables? `,
-        description: ``,
+        description: `All the fruits and vegetable produce that is purchased from the market can be rinsed in running water, followed by rubbing with hands. This is all that is needed to clean a majority of vegetables. The FDA recommends, "Rinse produce BEFORE you peel it, so dirt and bacteria aren't transferred from the knife onto the fruit or vegetable." `,
         videoSrc: `<iframe width="914" height="514" src="https://www.youtube.com/embed/zlsm4eZ-qVk" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
         type: 'youtube'
     },
--- client/src/components/user/home/Facts/index.tsx
+++ client/src/components/user/home/Facts/index.tsx
@@ -1,31 +1,41 @@
 import * as React from 'react';
 import styles from './styles.module.scss';
 import { factsData } from './factsData';
-import Navbar from '../../Navbar';
 
 export default class Facts extends React.Component<{}, {}> {
     public render() {
         return <>
-        <Navbar />
             <div className={styles.factsContainer}>
-
+                <div className={styles.heading}>Facts on Corona Virus</div>
                 {factsData && factsData.length ?
-                    factsData.map((data: any) => {
-                        return <div className={styles.facts}>
-                            <div className={styles.title}>{data.title}</div>
-                            <div className={styles.description}>{data.description}</div>
+                    factsData.map((data: any, index: number) => {
+                        // return <div className={styles.facts}>
+                        //     <div className={styles.title}>{data.title}</div>
+                        //     <div className={styles.description}>{data.description}</div>
+                        //     <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: data.videoSrc }}>
+                        //     </div>
+                        // </div>;
+                        if (index % 2 === 0) {
+                            return <div className={`${styles.container} ${styles.even}`}>
+                                <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: data.videoSrc }}>
+                                </div>
+                                <div className={styles.questionContainer}>
+                                    <div className={styles.title}>{data.title}</div><div className={styles.description}>{data.description}</div>
+                                </div>
+                            </div>;
+                        }
+                        return <div className={`${styles.container} ${styles.odd}`}>
+                            <div className={styles.questionContainer}>
+                                <div className={styles.title}>{data.title}</div><div className={styles.description}>{data.description}</div>
+                            </div>
                             <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: data.videoSrc }}>
                             </div>
                         </div>;
+
                     })
                     : ''
                 }
             </div>
         </>;
-    }
-
-
-    componentDidMount(){
-        window.scrollTo(0,0);
     }
 }
