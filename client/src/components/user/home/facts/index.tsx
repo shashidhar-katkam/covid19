c07f4f447338e1712391cad985ff574d13ea453c
Bimalendu Sarkar <bimalendu04@gmail.com>
2020-07-19 05:35:49 +0530

    Fact page style changed.

--- client/src/components/user/home/facts/index.tsx
+++ client/src/components/user/home/facts/index.tsx
@@ -6,21 +6,32 @@ export default class Facts extends React.Component<{}, {}> {
     public render() {
         return <>
             <div className={styles.factsContainer}>
-
+                <div className={styles.heading}>Facts on Corona Virus</div>
                 {factsData && factsData.length ?
-                    factsData.map((data: any) => {
-                        return <div className={styles.facts}>
-                            <div className={styles.title}>{data.title}</div>
-                            <div className={styles.description}>{data.description}</div>
+                    factsData.map((data: any, index: number) => {
+                        // return <div className={styles.facts}>
+                        //     <div className={styles.title}>{data.title}</div>
+                        //     <div className={styles.description}>{data.description}</div>
+                        //     <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: data.videoSrc }}>
+                        //     </div>
+                        // </div>;
+                        if (index % 2 === 0) {
+                            return <div className={`${styles.container} ${styles.even}`}>
+                                <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: data.videoSrc }}>
+                                </div>
+                                <div className={styles.questionContainer}>
+                                    <div className={styles.title}>{data.title}</div><div className={styles.description}>{data.description}</div>
+                                </div>
+                            </div>;
+                        }
+                        return <div className={`${styles.container} ${styles.odd}`}>
+                            <div className={styles.questionContainer}>
+                                <div className={styles.title}>{data.title}</div><div className={styles.description}>{data.description}</div>
+                            </div>
                             <div className={styles.videoContainer} dangerouslySetInnerHTML={{ __html: data.videoSrc }}>
-                                {/* <video className={styles.video} controls>
-
-                                    <source src={data.videoSrc} type="video/mp4" />
-                                        <source src={data.videoSrc} type="video/ogg" />
-                                            Your browser does not support HTML video.
-                                </video> */}
                             </div>
                         </div>;
+
                     })
                     : ''
                 }
