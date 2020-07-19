949fc9a9048dff91c411485d3b36a5e464dcfb0b
shashidhar <shashi2puppy@gmail.com>
2020-07-19 07:15:03 +0530

    sdfsdf

---
+++ client/build/precache-manifest.c37fc7d255ff18b3e912588071231396.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "493415826d524aa6a36bfd81027eb675",
+    "url": "/index.html"
+  },
+  {
+    "revision": "01eb7fb5af54f2297f01",
+    "url": "/static/css/2.e5b8e5b9.chunk.css"
+  },
+  {
+    "revision": "c7f9d9da64096f3c69e8",
+    "url": "/static/css/main.85c17549.chunk.css"
+  },
+  {
+    "revision": "01eb7fb5af54f2297f01",
+    "url": "/static/js/2.7cb4f8c9.chunk.js"
+  },
+  {
+    "revision": "c7f9d9da64096f3c69e8",
+    "url": "/static/js/main.e2057291.chunk.js"
+  },
+  {
+    "revision": "063482b99c2db0c77905",
+    "url": "/static/js/runtime-main.3c9ee553.js"
+  }
+]);
